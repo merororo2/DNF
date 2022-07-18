7c86f80286f4616317122f3ee71a16dff937412e
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-07-18 16:17:54 +0900

    캐릭터정보코딩

--- newdnfprojec/src/components/CharViewInfo.jsx
+++ newdnfprojec/src/components/CharViewInfo.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-
+import "../asset/css/charView.css";
+import Testpage from "./Testpage";
 function CharViewInfo(props) {
   const charDatas = props.charData;
-
+  const params = new URLSearchParams(window.location.search);
+  let serverNames = params.get("serverName");
+  let charName = params.get("chartersId");
   console.log(charDatas);
 
   return (
     <div className="charInfo">
-      <b></b>
+      <div className=" imgBakGround">
+        <img
+          src={`https://img-api.neople.co.kr/df/servers/${serverNames}/characters/${charName}?zoom=1`}
+          alt=""
+          className="charImg"
+        />
+      </div>
+
+      <span>레벨 : {charDatas.level}</span>
+      <span>각성명 : {charDatas.jobGrowName}</span>
+      <span> 직업 : {charDatas.jobName}</span>
       <b>길드 : {charDatas.guildName} </b>
       <b>모험단 : {charDatas.adventureName}</b>
     </div>
---
+++ newdnfprojec/src/components/Testpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Testpage() {
+  return <div>sadsdadsad</div>;
+}
+
+export default Testpage;
