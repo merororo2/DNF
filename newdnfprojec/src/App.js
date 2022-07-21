891c4f694cefa056f312a9c15a003823712754db
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-07-21 16:31:47 +0900

    코드수정

--- newdnfprojec/src/App.js
+++ newdnfprojec/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      {/* <SearchServer /> */}
       <Router>
         <Routes>
           <Route path="/" element={<SearchServer />} />
--- newdnfprojec/src/components/CharSearchForm.jsx
+++ newdnfprojec/src/components/CharSearchForm.jsx
@@ -3,23 +3,23 @@ import "../asset/css/searchForm.css";
 function CharSearchForm(props) {
   return (
     <div className="forms">
-      <form action="charView" method="get" name="forms">
-        <select name="serverName" id="serverName">
-          <option value="all">전체 </option>
-          {props.serverList.map((servers) => (
-            // console.log(servers)
-            <option
-              key={servers.serverId}
-              name={servers.serverId}
-              value={servers.serverId}
-            >
-              {servers.serverName}
-            </option>
-          ))}
-        </select>
-        <input type="search" name="charSearch" id="charSearch" />
-        <button>검색</button>
-      </form>
+      {/* <form action="charView" method="get" name="forms"> */}
+      <select name="serverName" id="serverName">
+        <option value="all">전체 </option>
+        {props.serverList.map((servers) => (
+          // console.log(servers)
+          <option
+            key={servers.serverId}
+            name={servers.serverId}
+            value={servers.serverId}
+          >
+            {servers.serverName}
+          </option>
+        ))}
+      </select>
+      <input type="search" name="charSearch" id="charSearch" />
+      <button>검색</button>
+      {/* </form> */}
     </div>
   );
 }
