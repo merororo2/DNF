8e79e772ff8504e9c12a01c9346f452086ca92d3
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-07-23 14:38:33 +0900

    탭구현/css수정

--- src/components/CharViewInfo.jsx
+++ src/components/CharViewInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "../asset/css/charView.css";
 import CharAvatar from "./CharAvatarData";
 import CharStatus from "./CharStatusData";
@@ -14,11 +14,12 @@ function CharViewInfo(props) {
   let charName = params.get("chartersId");
 
 
-  function test111() {
- let fdf = document.getElementsByClassName('navMenu');
-      
-   console.log(fdf);
-  }
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
 
 
   return (
@@ -38,10 +39,9 @@ function CharViewInfo(props) {
   <div className="mb-2 mx-2"> 길드 : {charDatas.guildName}</div>
   <div className="mb-2 mx-2">모험단 : {charDatas.adventureName}</div>
   <div className="mb-2 mx-2"> 레벨 : {charDatas.level}</div>
- 
-
 
   </div>
+
       {/* <ul className="navMenu">
         <li id ="equip" onClick={test111}><a href="#">장비</a></li>
         <li id ="avatar"><a href="#">아바타</a></li>
@@ -53,14 +53,103 @@ function CharViewInfo(props) {
       
     
 
+
+ <div className="container">
+      <div className="bloc-tabs">
+        <button
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          장비
+        </button>
+        <button
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          아바타
+        </button>
+        <button
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+          휘장
+        </button>
+        <button
+          className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(4)}>
+        
+          탈리스만
+        </button>
+        <button
+          className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(5)}
+        >
+          능력치
+        </button>
+        <button
+          className={toggleState === 6 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(6)}
+        >
+          Tab 6
+        </button>
+      </div>
+
+      <div className="content-tabs">
+        <div
+          className={toggleState === 1 ? "content  active-content" : "content"}
+        >
+          <h2>장비</h2>
+          <CharItem/>
+        
+        </div>
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "content"}
+        >
+          <h2>아바타</h2>
+            <CharAvatar />
+        </div>
+
+        <div
+          className={toggleState === 3 ? "content  active-content" : "content"}
+        >
+          <h2>크리쳐</h2>
+            
+      <CharCreature />
+        </div>
+        
+        <div
+          className={toggleState === 4 ? "content  active-content" : "content"}
+        >
+          <h2>휘장</h2>
+            
+      <CharFlag />
+        </div>
+        
+        <div
+          className={toggleState === 5 ? "content  active-content" : "content"}
+        >
+          <h2>탈리스만</h2>
+            
+           
+      <CharTalisman />
+        </div>
+        
+        <div
+          className={toggleState === 6 ? "content  active-content" : "content"}
+        >
+          <h2>능력치</h2>
+            
+          <CharStatus />
+        </div>
+      </div>
+    </div>
     
     
 
-      <CharStatus />
-      {/* <CharAvatar /> */}
-      {/* <CharCreature /> */}
-      {/* <CharFlag /> */}
-      {/* <CharTalisman /> */}
+   
+
+
       </div>
   );
 }
